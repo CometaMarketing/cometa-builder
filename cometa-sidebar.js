70545ab831badbf3aeb0ff3fb64bdce215a4abde
CometaMarketing <contato.cometamkt@gmail.com>
2024-10-03 12:42:06 -0300

    Update cometa-sidebar.js

--- cometa-sidebar.js
+++ cometa-sidebar.js
@@ -6,7 +6,7 @@ jQuery(document).ready(function($) {
         $('body').append(`
             <div id="cometa-sidebar">
                 <div id="cometa-toggle-bar">⧽</div>
-                <iframe src="https://buider.cometamarketing.com.br/biblioteca" style="width: 100%; height: 100%; border: none;"></iframe>
+                <iframe src="https://builder.cometamarketing.com.br/biblioteca" style="width: 100%; height: 100%; border: none;"></iframe>
             </div>
         `);
     }
